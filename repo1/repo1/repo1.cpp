cce9890c029ebb8104253a5a2d81b4e7dd91434a
PIBO0122023 Хусаинов Надим Рамильевич <PIBO0122023@sibupk.su>
2024-02-10 20:26:43 +0700

    kodik

--- repo1/repo1/repo1.cpp
+++ repo1/repo1/repo1.cpp
@@ -1,17 +1,21 @@
 ﻿#include <iostream>
 using namespace std;
 
-bool isLeapYear(int year)
-{
-    if (year % 4 == 0)
-    {
-        return true;
-    }
-    else
-    {
+bool isLeapYear(int year) {
+    if (year % 4 != 0) {
         return false;
     }
+    else if (year % 100 != 0) {
+        return true;
+    }
+    else if (year % 400 != 0) {
+        return false;
+    }
+    else {
+        return true;
+    }
 }
+
 
 int main()
 {
